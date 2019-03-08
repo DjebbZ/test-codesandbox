6867fd07adc59f4d63ec26239b745da42e1b852b
Khalid Jebbari <khalid.jebbari@gmail.com>
2019-03-08 14:15:53 +0100

    Add readOnly attribute (from Codesandbox)
    Codesandbox rocks. Shit is crazy.

--- src/index.js
+++ src/index.js
@@ -10,12 +10,13 @@ function App() {
   React.useEffect(() => {
     setTimeout(() => setState("2"), 2000);
   }, []);
+  console.log(state);
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <p>Code added to see if CodeSandbox commits to GitHub.</p>
-      <select value={state}>
+      <select value={state} readOnly>
         <option value="placeholder">Marque</option>
         {options.map(opt => (
           <option key={opt} value={opt}>
